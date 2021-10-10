eff7ea846d231018dc309314b4a16a8ab9d55a1e
Lia OBrien <liaobrien123@gmail.com>
2021-10-10 13:53:35 -0400

    created inquirer questions and started init function

--- index.js
+++ index.js
@@ -2,9 +2,130 @@ const inquirer = require('inquirer');
 const mysql = require('mysql2');
 const cTable = require('console.table');
 
-var db = mysql.createConnection({
+const db = mysql.createConnection({
       host: process.env.DB_HOST,
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: "employees_db"
 });
+
+const firstQuestion = [
+      {
+            type: "list",
+            message: "What would you like to do?",
+            choices: [
+                  "View All Employees",
+                  "Add Employee",
+                  "Update Employee Role",
+                  "View All Roles",
+                  "Add Role",
+                  "View All Departments",
+                  "Add Department",
+                  "Quit"],
+            name: "toDo"
+      }
+];
+
+const addDept = [
+      {
+            type: "input",
+            message: "What department would you like to add?",
+            name: "addDept"
+      }
+];
+
+const addRole = [
+      {
+            type: "input",
+            message: "What role would you like to add?",
+            name: "addRole"
+      },
+      {
+            type: "input",
+            message: "What is the salary for this role?",
+            name: "roleSalary"
+      },
+      {
+            type: "list",
+            message: "Which department does the role belong to?",
+            choices: [
+                  "Engineering",
+                  "Finance",
+                  "Legal",
+                  "Sales",
+                  "Service"
+            ],
+            name: "roleDept"
+      }
+];
+
+const addEmp = [
+      {
+            type: "input",
+            message: "What is the employee's first name?",
+            name: "empFirstName"
+      },
+      {
+            type: "input",
+            message: "What is the employee's last name?",
+            name: "empLastName"
+      },
+      {
+            type: "list",
+            message: "What is the employee's role?",
+            choices: [
+                  // will create later through seeds.sql
+            ],
+            name: "empRole"
+      },
+      {
+            type: "list",
+            message: "Who is the employee's manager?",
+            choices: [
+                  // will create later through seeds.sql
+            ],
+            name: "empManager"
+      }
+];
+
+const updateEmpRole = [
+      {
+            type: "list",
+            message: "Which Employee would you like to update",
+            choices: [],
+            name: "empUpdateRole",
+      },
+
+      {
+            type: "input",
+            name: "empNewRole",
+            message: "What is the Employee's new role",
+      },
+];
+
+function startApp() {
+      inquirer
+            .prompt(firstQuestion)
+            .then((response) => {
+                  switch (response) {
+                        case "Add Employee":
+                              // actions tbd atm
+                              break;
+
+                        case "Update Employee Role":
+                              break;
+
+                        case "View All Roles":
+                              break;
+
+                        case "Add Role":
+                              break;
+
+                        case "View All Departments":
+                              break;
+
+                        case "Quit":
+                              break;
+                  }
+            })
+};
