b6f34af781c6453859f4f68eb0bbd706ad163edb
Lia OBrien <liaobrien123@gmail.com>
2021-10-10 14:59:40 -0400

    filled out seeds.sql and added contents to my tables

--- db/schema.sql
+++ db/schema.sql
@@ -5,7 +5,7 @@ USE employees_db;
 
 DROP TABLE IF EXISTS department;
 CREATE TABLE department (
-      id INT NOT NULL,
+      id INT AUTO_INCREMENT,
       name VARCHAR(30) NOT NULL,
       PRIMARY KEY (id)
 );
@@ -13,22 +13,25 @@ CREATE TABLE department (
 
 DROP TABLE IF EXISTS role;
 CREATE TABLE role (
-      id INT NOT NULL,
+      id INT AUTO_INCREMENT,
       title VARCHAR(30) NOT NULL,
       salary DECIMAL NOT NULL,
-      department_id INT NOT NULL,
+      department_id INT,
       PRIMARY KEY (id),
       FOREIGN KEY (department_id)
       REFERENCES department(id)
+      ON DELETE SET NULL
 );
 
 DROP TABLE IF EXISTS employee;
 CREATE TABLE employee (
-      id INT NOT NULL,
+      id INT AUTO_INCREMENT,
       first_name VARCHAR(30) NOT NULL,
       last_name VARCHAR(30) NOT NULL,
-      role_id INT NOT NULL,
+      role_id INT,
       manager_id INT NOT NULL,
+      PRIMARY KEY (id),
       FOREIGN KEY (role_id)
       REFERENCES role(id)
+      ON DELETE SET NULL
 );
---
+++ db/seeds.sql
@@ -0,0 +1,25 @@
+INSERT INTO department(name)
+VALUES ("Engineering"),
+       ("Finance"),
+       ("Human Resources"),
+       ("Sales"),
+       ("IT");
+
+INSERT INTO role(title, salary, department_id)
+VALUES ("Senior Engineer", 100000, 1),
+       ("Junior Engineer", 70000, 1),
+       ("Accounting Manager", 100000, 2),
+       ("Accountant", 70000, 2),
+       ("HR Manager", 80000, 3),
+       ("HR Specialist", 50000, 3),
+       ("Sales Manager", 100000, 4),
+       ("Salesperson", 70000, 4),
+       ("IT Manager", 80000, 5),
+       ("IT Specialist", 50000, 5);
+
+INSERT INTO employee(first_name, last_name, role_id, manager_id)
+VALUES ("Elaine", "Walters", 1, 1),
+       ("Rafael", "Allison", 2, 2),
+       ("Jeffery", "Fisher", 3, 3),
+       ("Mabel", "Anderson", 4, 4),
+       ("Chris", "Bates", 5, 5);
